d9ac0a7216c2dbdf43f8c9f5c351d5d50b544113
runalb <65166862+runalb@users.noreply.github.com>
2022-07-27 18:25:54 +0530

    54

--- S9 Django-Views-Routing-URLs/my_site/first_app/urls.py
+++ S9 Django-Views-Routing-URLs/my_site/first_app/urls.py
@@ -3,4 +3,16 @@ from . import views
 
 urlpatterns = [
     path("sv/",views.simple_view),
+
+    # ---- Dynamic urls: ----
+    
+    # /123 intiger urls
+    path("<int:value>",views.int_view),
+    # /sting_value urls
+    path("<str:value>",views.string_view),
+    # /urls-with-dashes
+    path("<slug:value>",views.slug_view) ,
+
+
+
 ]
--- S9 Django-Views-Routing-URLs/my_site/first_app/views.py
+++ S9 Django-Views-Routing-URLs/my_site/first_app/views.py
@@ -6,3 +6,11 @@ from django.http.response import HttpResponse
 def simple_view(request):
     return HttpResponse("Simple View")
 
+def string_view(request,value):
+    return HttpResponse(f"String: {value}")
+
+def int_view(request,value):
+    return HttpResponse(f"intiger: {value}")
+
+def slug_view(request,value):
+    return HttpResponse(f"slug: {value}")
